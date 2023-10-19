5d43210113af368e5eb0daa95840568ad26852c6
Juan-CCU <142257499+Juan-CCU@users.noreply.github.com>
2023-10-19 17:22:59 -0300

    PRUEBA EN local.py

--- integraSoft_rest/integraSoft_rest/settings/local.py
+++ integraSoft_rest/integraSoft_rest/settings/local.py
@@ -3,7 +3,7 @@ from .base import *
 from decouple import config
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# prueba SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ["192.168.0.107", "127.0.0.1", "0.0.0.0"]

ae8279df014a277bb23f382ebb2e6420f4dbb6db
Juan-CCU <142257499+Juan-CCU@users.noreply.github.com>
2024-01-17 12:34:22 -0300

    Update manage.py

--- integraSoft_rest/manage.py
+++ integraSoft_rest/manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""PRUEBA ENERO END Django's command-line utility for administrative tasks."""
+"""PRUEBA ENERO END 1 Django's command-line utility for administrative tasks."""
 import os
 import sys
 

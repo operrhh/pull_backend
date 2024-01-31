3f9e2ddf07861eb87698e6a660e07a02c9e97841
Luciano <lucian.pino.a@gmail.com>
2024-01-31 11:37:04 -0300

    Add dynamic page size configuration to CustomPaginationPeopleSoft

--- integraSoft_rest/apps/workers/pagination.py
+++ integraSoft_rest/apps/workers/pagination.py
@@ -1,6 +1,31 @@
 from rest_framework.pagination import PageNumberPagination
+from apps.parameters.models import Parameter, ParameterType
 
 class CustomPaginationPeopleSoft(PageNumberPagination):
-    page_size = 200  # Número de elementos por página
-    page_size_query_param = 'page_size'
-    max_page_size = 1000  # Número máximo de elementos por página
+
+    def __init__(self):
+        super().__init__()
+        self.setup_parameters()
+
+    def setup_parameters(self):
+        # Obtener el ID del tipo de parámetro para 'size_request'
+        parameter_type_id = ParameterType.objects.filter(Description='size_request').values_list('id', flat=True).first()
+
+        # Obtener el tamaño de la solicitud de PeopleSoft de la tabla de parámetros
+        parameter = Parameter.objects.filter(
+            ParameterTypeId=parameter_type_id,
+            Enabled=True,
+            FilterField1='size_request',
+            FilterField2='peoplesoft',
+            FilterField3='worker'
+        ).first()
+        
+        # Configurar el tamaño de la página
+        if parameter:
+            self.page_size = int(parameter.Value)  # Tamaño de la página
+        else:
+            # Si no se encuentra el parámetro, usar un valor predeterminado
+            self.page_size = 10  # Valor predeterminado
+
+        self.page_size_query_param = 'page_size'
+        self.max_page_size = 1000  # Número máximo de elementos por página
